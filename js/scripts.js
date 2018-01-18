72408b70e2d473e631996456a8f3faa4f04caa41
Cameron Anderson and Russell Hofvendahl <cameron@>
2018-01-18 11:57:02 -0800

    add russ method, both multibase

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,38 @@
-function camsWay(number) {
+function digitToChar(dig){
+  if (dig < 10) {
+    return dig + "";
+  }
+  else {
+    return "abcdefghijklmnop".charAt(dig - 10);
+  }
+}
+
+
+function camsWay(base, number) {
   var highestPower = 1;
-  var bits = "";
-  for (var i = 0; highestPower * 2 < number; i += 1) {
-    highestPower *= 2;
+  var digits = "";
+  for (var i = 0; highestPower * base < number; i += 1) {
+    highestPower *= base;
   }
   while(highestPower >= 1 ) {
-    if (number >= highestPower) {
-      number -= highestPower;
-      bits += "1";
-    } else {
-      bits += "0";
-    }
-    highestPower /= 2;
+    var dig = Math.floor(number / highestPower);
+    number -= dig * highestPower;
+    digits += digitToChar(dig);
+    highestPower /= base;
+    console.log(dig);
   }
-  return bits;
+  return digits;
+}
+
+function russWay(base, lump) {
+  var digits = "";
+  while (lump !== 0) {
+    console.log(lump%base);
+    digits = digitToChar(lump%base) + digits;
+    lump -= lump%base;
+    lump /= base;
+  }
+  return digits;
 }
 
 
@@ -21,5 +40,6 @@ $(document).ready(function() {
   // var base = prompt("Enter a Base...")
   // var number = prompt("Enter a Number...")
 
-  alert(camsWay(38));
+  alert(camsWay(16, 255));
+  alert(russWay(16, 255));
 });
